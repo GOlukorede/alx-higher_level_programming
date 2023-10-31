c851e1beceee0edca5404f1989638282e3b61be1
GOlukorede <samsongreats@gmail.com>
2023-10-31 13:08:17 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    """
-    This function prints the fizzbuzz sequence
-    """
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz", end=" ")
