4a4c8c1f27b2574f36d55b19dc13e32e4d305d58
GOlukorede <samsongreats@gmail.com>
2023-11-02 11:32:11 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,17 @@
 
 if __name__ == "__main__":
     import sys
+
+    # Function to add two numbers
     argc = len(sys.argv) - 1
     argv = sys.argv[1:]
 
     if (argc == 0):
-      print("{} arguments.".format(argc))
+        print("{} arguments.".format(argc))
     elif (argc == 1):
-      print("{} argument:".format(argc))
-      print("{}: {}".format(argc, argv[0]))
+        print("{} argument:".format(argc))
+        print("{}: {}".format(argc, argv[0]))
     else:
-      print("{} arguments:".format(argc))
-      for i in range(argc):
-        print("{}: {}".format(i + 1, argv[i]))
+        print("{} arguments:".format(argc))
+        for i in range(argc):
+            print("{}: {}".format(i + 1, argv[i]))
