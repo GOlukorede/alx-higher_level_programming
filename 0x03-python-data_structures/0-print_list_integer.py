4b4b923e95c8e8e629ba911042f7dfe36588e3ac
GOlukorede <samsongreats@gmail.com>
2023-11-03 01:43:47 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@
 if __name__ == "__main__":
 
     def print_list_integer(my_list=[]):
-        for list in range(len(my_list)):
-            print(my_list[list])
+        for item in my_list:
+            print(item)
