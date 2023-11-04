25bc6227bd2918912a6a806e9a190752fedccf1a
GOlukorede <samsongreats@gmail.com>
2023-11-04 03:26:15 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-
-    def no_c(my_string):
-        final = ""
-        for item in my_string:
-            if item == 'C' or item == 'c':
-                continue
-            else:
-                final = final + item
-        return final
+def no_c(my_string):
+    final = ""
+    for item in my_string:
+        if item == 'C' or item == 'c':
+            continue
+        else:
+            final = final + item
+            return final
