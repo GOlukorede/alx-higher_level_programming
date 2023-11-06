6634f5db901e616e7466f20bfae39e1be913fcfa
GOlukorede <samsongreats@gmail.com>
2023-11-06 00:01:57 -0800

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j,value in enumerate(i):
+            if j > 0:
+                print(" ", end="")
+            print("{:d}".format(value), end="")
+        print()
