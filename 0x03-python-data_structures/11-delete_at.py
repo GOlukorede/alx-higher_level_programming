ecf82a8dcfff2f699ee00a616247cf347d4a37f1
GOlukorede <samsongreats@gmail.com>
2023-11-06 07:09:33 -0800

    11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = []
+
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+
+    my_list.clear()
+
+    for item in new_list:
+        my_list.append(item)
+    return my_list
