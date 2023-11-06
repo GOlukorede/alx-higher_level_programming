2e9c83e485d677eb23504aa76b4906c79a69b3d5
GOlukorede <samsongreats@gmail.com>
2023-11-06 04:41:46 -0800

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    for i in my_list:
+        if i % 2 == 0:
+            my_list[i] = True
+        else:
+            my_list[i] = False
+    return my_list
