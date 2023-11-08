15ecd3f29eaad9ad34c42973e0a4aac72e67310d
GOlukorede <samsongreats@gmail.com>
2023-11-08 05:01:00 -0800

    README.md

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    for row in matrix:
+        squared = [element ** 2 for element in row]
+        squared_matrix.append(squared)  
+    return squared_matrix
