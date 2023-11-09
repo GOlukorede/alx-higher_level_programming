22690a686b2f4b6afdf1a387a2289313291ca223
GOlukorede <samsongreats@gmail.com>
2023-11-09 00:20:27 -0800

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * dlistint_len - Count number of elements in a list
+ * @h: node
+ *
+ * Return: Number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t sum = 0;
+while (h != NULL)
+{
+sum += 1;
+h = h->next;
+}
+printf("%lu elements\n", sum);
+return (sum);
+}
