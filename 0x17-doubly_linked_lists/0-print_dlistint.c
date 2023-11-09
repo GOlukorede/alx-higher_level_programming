50a76d96f58bf9af622870d1049055b0530734b2
GOlukorede <samsongreats@gmail.com>
2023-11-09 00:00:59 -0800

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "list.h"
+
+/**
+ * print_dlistint - print values of doubly linked list
+ * @h: node
+ *
+ * Return: Number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+int data;
+size_t sum = 0;
+while (h != NULL)
+{
+sum += 1;
+data = h->n;
+printf("%d\n", data);
+h = h->next;
+}
+printf("%d elements\n", sum);
+return (sum);
+}
