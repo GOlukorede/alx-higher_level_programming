b6f75027f4a1f508d4e23886a4a3e291ba526805
GOlukorede <samsongreats@gmail.com>
2023-11-20 07:22:52 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    div = 0
+    result = None
+    try:
+        div = int(a / b)
+        result = div
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {:d}".format(div))
+        return result
