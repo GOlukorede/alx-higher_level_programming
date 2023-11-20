a29002df9f0abc927e8cd3ea07bb936c589f4b44
GOlukorede <samsongreats@gmail.com>
2023-11-20 07:46:27 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,5 +9,8 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        print("Inside result: {:d}".format(div))
+        if result is None:
+            print("Inside result: {}".format(None))
+        else:
+            print("Inside result: {:.1f}".format(div))
         return result
