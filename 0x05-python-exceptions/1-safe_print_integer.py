e6bab745f237f93c13dc2194104874d5b8afb067
GOlukorede <samsongreats@gmail.com>
2023-11-20 05:14:45 -0800

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if "{:d}".format(value):
-            print("{:d}".format(value))
-            return (True)
+        print("{:d}".format(value))
+        return (True)
     except TypeError, ValueError:
         return (False)
