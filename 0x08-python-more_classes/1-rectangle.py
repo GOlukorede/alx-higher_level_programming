e3bec8157ce13dfe6572459f6bc0160a3c607221
GOlukorede <samsongreats@gmail.com>
2023-11-27 07:40:08 -0800

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class Rectangle that defines a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self._Rectangle__width = width
+        self._Rectangle__height = height
+
+    @property
+    def width(self):
+        return self._Rectangle__width
+      
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._Rectangle__width = value
+
+    @property
+    def height(self):
+        return self._Rectangle__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._Rectangle__height = value
