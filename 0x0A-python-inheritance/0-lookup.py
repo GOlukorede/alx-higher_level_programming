55a79f6020182ef47caef41075f0b0de76c03138
GOlukorede <samsongreats@gmail.com>
2023-12-05 07:32:40 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A function that returns the list of available attributes and methods of an object"""
+"""A function that returns the list of available 
+   attributes and methods of an object"""
 
 
 def lookup(obj):
