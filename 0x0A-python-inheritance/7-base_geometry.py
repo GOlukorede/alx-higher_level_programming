a4e677baecf27714fbd32814335827f7f5305cdb
GOlukorede <samsongreats@gmail.com>
2023-12-06 01:15:43 -0800

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,7 +5,7 @@ A module that contains class BaseGeometry
 
 
 class BaseGeometry:
-    """A class that raises an exception when called"""""
+    """A class that raises an exception when called"""
     def area(self):
         """Function that raises an exception when called"""
         raise Exception("area() is not implemented")
