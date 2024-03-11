870fb188d63a7fe9cde4dfafe14f53d201beb719
GOlukorede <samsongeats@gmail.com>
2024-03-11 05:42:06 -0700

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const [, , ...args] = process.argv;
+const number = parseFloat(args[0]);
+if (Number.isFinite(number)) {
+	const test = Math.trunc(number);
+	console.log(`My number: ${test}`);
+} else {
+	console.log('Not a number');
+};
